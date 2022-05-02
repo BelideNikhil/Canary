55707424e85bac5840655de0410a9f5be6b37a22
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2022-05-02 20:49:51 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 
-// Call make Server
 makeServer();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
