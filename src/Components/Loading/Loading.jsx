e073faeb330ec1fceab551cc6038083818cc762c
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2022-05-11 14:47:55 +0530

    add:posts-db, loading, dark-mode, ref:store

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,11 +1,11 @@
 import "./Loading.css";
 export default function Loading() {
     return (
-        <div className="lds-ring inline-block relative w-10	h-10">
-            <div className="absolute rounded-full box-border w-6 h-6"></div>
-            <div className="absolute rounded-full box-border w-6 h-6"></div>
-            <div className="absolute rounded-full box-border w-6 h-6"></div>
-            <div className="absolute rounded-full box-border w-6 h-6"></div>
+        <div className="lds-ring inline-block relative w-20	h-20 ">
+            <div className="absolute rounded-full box-border w-16 h-16"></div>
+            <div className="absolute rounded-full box-border w-16 h-16"></div>
+            <div className="absolute rounded-full box-border w-16 h-16"></div>
+            <div className="absolute rounded-full box-border w-16 h-16"></div>
         </div>
     );
 }
