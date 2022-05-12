98172a5821f6da505e0a805293676468c066cdc8
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2022-05-13 01:19:40 +0530

    refactor: Utils, Barrel-exp, routes

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,11 @@
 import Loading from "./Loading/Loading";
 import Sidebar from "./Sidebar/Sidebar";
+import Suggestions from "./Suggestions/Suggestions";
+import Feed from "./Feed/Feed";
+import FollowListModal from "./FollowListModal/FollowListModal";
+import UserAvatar from "./UserAvatar/UserAvatar";
+import PostCard from "./PostCard/PostCard";
+import EditModal from "./EditModal/EditModal";
+import OptionsModal from "./OptionsModal/OptionsModal";
 
-export { Loading, Sidebar };
+export { Loading, Sidebar, Suggestions, Feed, FollowListModal, UserAvatar, PostCard, EditModal, OptionsModal };
