94311d683ace4bf55909ecfc80bd0080478fdf28
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2022-05-15 20:52:46 +0530

    refactor: barrel-exports

--- src/features/Post/Components/index.js
+++ src/features/Post/Components/index.js
@@ -1,5 +1,6 @@
 import Feed from "../../../Components/Feed/Feed";
 import NewPost from "./NewPost";
 import PostCard from "../../../Components/PostCard/PostCard";
+import FilterBar from "./FilterBar";
 
-export { Feed, NewPost, PostCard };
+export { Feed, NewPost, PostCard, FilterBar };
