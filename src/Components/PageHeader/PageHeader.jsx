bc84ff94ff76a82268e7fa0596459180e11a80c3
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2022-05-16 20:59:07 +0530

    add: search-feat

--- src/Components/PageHeader/PageHeader.jsx
+++ src/Components/PageHeader/PageHeader.jsx
@@ -1,18 +1,52 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
 import { toggleTheme } from "../../features/User/userSlice";
+import { Search } from "../index";
 
 export default function PageHeader({ pagename }) {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const { darkTheme } = useSelector((state) => state.user);
+    const { pathname } = useLocation();
+
     return (
-        <div className="flex z-9 p-3 sticky top-0 bg-slate-200 dark:bg-gray-900 border-b border-slate-500">
-            <div className=" font-semibold  text-slate-800  dark:text-slate-100 w-full">{pagename}</div>
-            <button
-                className={`w-6 h-6 transition duration-[0.5s] dark:text-slate-200 ${!darkTheme ? "rotate-180" : ""}`}
-                onClick={() => dispatch(toggleTheme(darkTheme ? false : true))}
-            >
-                <span className="material-icons-outlined">{darkTheme ? "dark_mode" : "light_mode"}</span>
-            </button>
+        <div className="flex z-9 p-3 sticky top-0 bg-slate-200 dark:bg-gray-900 border-b border-slate-500 flex flex-col">
+            <div className="flex justify-between items-center mb-4 lg:mb-0">
+                <div className="flex">
+                    {pathname.includes("post") || pathname.includes("profile") ? (
+                        <button
+                            className="text-slate-800 dark:text-slate-200 mr-2 w-6 h-6"
+                            onClick={() => navigate(-1)}
+                        >
+                            <span className="material-icons-outlined">arrow_back</span>
+                        </button>
+                    ) : null}
+                    <div className=" font-semibold  text-slate-800  dark:text-slate-100 ">
+                        {pagename === "Home" ? (
+                            <>
+                                <div role="button" className="md:hidden">
+                                    <img src="/Canary.png" alt="" className="w-10 h-10" />
+                                </div>
+                                <span className="hidden md:block">{pagename}</span>
+                            </>
+                        ) : (
+                            pagename
+                        )}
+                    </div>
+                </div>
+
+                <button
+                    className={`w-6 h-6 transition duration-[0.5s] dark:text-slate-200 ${
+                        !darkTheme ? "rotate-180" : ""
+                    }`}
+                    onClick={() => dispatch(toggleTheme(darkTheme ? false : true))}
+                >
+                    <span className="material-icons-outlined">{darkTheme ? "dark_mode" : "light_mode"}</span>
+                </button>
+            </div>
+            <div className="block lg:hidden">
+                <Search />
+            </div>
         </div>
     );
 }
---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,47 @@
+import { debounce, GetSearch } from "../../Utils";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import UserAvatar from "../UserAvatar/UserAvatar";
+import { setSearchText } from "../../features/User/userSlice";
+
+export default function Search() {
+    const [filteredUsers, setFilteredUsers] = useState([]);
+    const { searchText, users } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (searchText) {
+            setFilteredUsers(GetSearch(users, searchText));
+        }
+    }, [searchText, users]);
+
+    return (
+        <div className="relative">
+            <div className=" rounded-full overflow-hidden border-2 border-slate-500 focus-within:border-primary-color">
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <input
+                        className="w-full px-3 py-1  focus:outline-none text-sm bg-slate-100 dark:bg-slate-900 text-slate-800 dark:text-slate-200 placeholder-shown:text-sm"
+                        placeholder="Search User..."
+                        type="search"
+                        onChange={debounce((e) => dispatch(setSearchText(e.target.value.trim())))}
+                    />
+                </form>
+            </div>
+            {searchText ? (
+                <div className="absolute top-10 border-2 left-1/2 -translate-x-1/2 border-slate-700 dark:border-slate-300 bg-slate-200 dark:bg-slate-700 rounded-md w-80 lg:w-full shadow-2xl p-2">
+                    {filteredUsers?.length > 0 ? (
+                        filteredUsers.map((user) => {
+                            return (
+                                <div key={user._id} className="my-2">
+                                    <UserAvatar username={user.username} />
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <div className="text-slate-800 dark:text-slate-300 text-md">No users found...</div>
+                    )}
+                </div>
+            ) : null}
+        </div>
+    );
+}
